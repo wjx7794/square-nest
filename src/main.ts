97240489710775e7fd1b9f1bdf6613e727d00d5e
wjx7794 <wjx7794@163.com>
2024-11-21 10:49:46 +0800

    feat: tz

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
 import * as cookieParser from 'cookie-parser';
+import * as compression from 'compression';
+import rateLimit from 'express-rate-limit';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 // 外部模块
@@ -21,6 +23,26 @@ async function bootstrap() {
 
   // 对所有传入的「客户端有效负载」强制执行验证规则
   app.useGlobalPipes(new ValidationPipe());
+
+  // 压缩以减小响应主体的大小
+  app.use(compression());
+
+  // 允许跨域
+  app.enableCors();
+
+  // 设置限速，每个IP地址每分钟最多允许50个请求
+  app.use(
+    rateLimit({
+      // 限制窗口的持续时间(以毫秒为单位)
+      windowMs: 60 * 1000,
+      // 在给定窗口持续时间内可以发出的最大请求数
+      limit: 50,
+      // 用户在超出限制时收到的响应消息
+      message: '请求过于频繁，请稍后再试',
+      // 响应头上展示剩余次数
+      headers: true,
+    }),
+  );
 
   // 监听端口启动
   await app.listen(PORT);
